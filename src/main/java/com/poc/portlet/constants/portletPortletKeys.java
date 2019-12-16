9f0c0c2edcdd960c97a36bfbe712b1651491670d
Gaalator <gaalator@locosporelcodigo.com>
2019-12-16 16:26:31 -0600

    First commit

---
+++ src/main/java/com/poc/portlet/constants/portletPortletKeys.java
@@ -0,0 +1,11 @@
+package com.poc.portlet.constants;
+
+/**
+ * @author Gaalator
+ */
+public class portletPortletKeys {
+
+	public static final String PORTLET =
+		"com_poc_portlet_portletPortlet";
+
+}
---
+++ src/main/java/com/poc/portlet/portlet/portletPortlet.java
@@ -0,0 +1,30 @@
+package com.poc.portlet.portlet;
+
+import com.poc.portlet.constants.portletPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Gaalator
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=Gluo",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + portletPortletKeys.PORTLET,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class portletPortlet extends MVCPortlet {
+}
